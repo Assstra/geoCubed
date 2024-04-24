12615dff016120a3a6817657bce236db92f4f789
Adrien Raimbault <adrien.raimbault@etu.umontpellier.fr>
2024-04-24 11:08:14 +0200

    feat(map): add GoogleMap API to project on screen3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
 }
 
 android {
@@ -60,6 +61,19 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation("androidx.compose.ui:ui:1.6.4")
     implementation("androidx.navigation:navigation-compose:2.7.7")
+
+    implementation ("com.google.maps.android:maps-compose:4.4.0")
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+
+    // Optionally, you can include the Compose utils library for Clustering,
+    // Street View metadata checks, etc.
+    //implementation ("com.google.maps.android:maps-compose-utils:4.4.1")
+
+    // Optionally, you can include the widgets library for ScaleBar, etc.
+    //implementation ("com.google.maps.android:maps-compose-widgets:4.4.1")
+
+    implementation ("androidx.activity:activity-ktx:1.9.0")
+    implementation ("androidx.fragment:fragment-ktx:1.6.2")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,13 @@ plugins {
     id("com.android.application") version "8.2.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
+    }
+}
