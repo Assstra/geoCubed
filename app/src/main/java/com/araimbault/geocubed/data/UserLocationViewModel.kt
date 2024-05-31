40cede791592e66d0de5782eddbdf2c0c0e1364b
Adrien Raimbault <adrien.raimbault@etu.umontpellier.fr>
2024-05-31 13:28:10 +0200

    feat: create UserLocationViewModel and add logic to retrieve user position

--- app/src/main/java/com/araimbault/geocubed/data/UserLocationViewModel.kt
+++ app/src/main/java/com/araimbault/geocubed/data/UserLocationViewModel.kt
@@ -1,25 +1,79 @@
 package com.araimbault.geocubed.data
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Looper
+import androidx.activity.viewModels
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.ViewModel
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.Priority
 import com.google.android.gms.maps.model.LatLng
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import java.lang.reflect.Constructor
 
 data class UserLocationState(
     val lat: Double = 0.0,
     val long: Double = 0.0,
 )
 
-class UserLocationViewModel: ViewModel() {
+class UserLocationViewModel(private val fusedLocationClient: FusedLocationProviderClient) :
+    ViewModel() {
+
     private val _currentLocation = MutableStateFlow(UserLocationState())
     val currentLocation: StateFlow<UserLocationState> = _currentLocation.asStateFlow()
 
-    fun updateLocation(newLatLng: LatLng) {
-        _currentLocation.update { currentState -> currentState.copy(
-            lat = newLatLng.latitude,
-            long = newLatLng.longitude,
-        )}
+    private val locationRequest = LocationRequest.Builder(1000) // 1 second interval
+        .setPriority(Priority.PRIORITY_HIGH_ACCURACY)
+        .build()
+
+    private val locationCallback =
+        object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                super.onLocationResult(locationResult)
+                locationResult.lastLocation?.let {
+                    updateLocation(
+                        LatLng(
+                            it.latitude,
+                            it.longitude
+                        )
+                    )
+                }
+            }
+        }
+    private fun startLocationUpdates() {
+        fusedLocationClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            Looper.getMainLooper()
+        )
+    }
+
+    private fun updateLocation(newLatLng: LatLng) {
+        _currentLocation.update { currentState ->
+            currentState.copy(
+                lat = newLatLng.latitude,
+                long = newLatLng.longitude,
+            )
+        }
+    }
+
+    private fun stopLocationUpdates() {
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
+
+    override fun onCleared() {
+        stopLocationUpdates()
+    }
+
+    init {
+        startLocationUpdates()
     }
 }
--- app/src/main/java/com/araimbault/geocubed/ui/components/map/MapScreen.kt
+++ app/src/main/java/com/araimbault/geocubed/ui/components/map/MapScreen.kt
@@ -4,46 +4,41 @@ import android.location.Location
 import android.widget.Toast
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavController
-import com.google.android.gms.location.FusedLocationProviderClient
+import com.araimbault.geocubed.data.UserLocationViewModel
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.tasks.OnSuccessListener
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
 
 @Composable
-fun MapScreen(navController: NavController, fusedLocationProviderClient: FusedLocationProviderClient) {
+fun MapScreen(
+    navController: NavController,
+    userLocationViewModel: UserLocationViewModel
+) {
 
-    val singapore = LatLng(1.35, 103.87)
+    val state = userLocationViewModel.currentLocation.collectAsState()
+    val userLocation = LatLng(state.value.lat, state.value.long)
+
     val cameraPositionState = rememberCameraPositionState {
-        position = CameraPosition.fromLatLngZoom(singapore, 10f)
+        position = CameraPosition.fromLatLngZoom(userLocation, 10f)
     }
-
-    val current = LocalContext.current
-
-
-    fusedLocationProviderClient.lastLocation
-        .addOnSuccessListener {
-            // Got last known location. In some rare situations this can be null.
-            println(it)
-            Toast.makeText(
-                current,
-                "Approximate location access already granted.",
-                Toast.LENGTH_SHORT
-            ).show()
-        }
 
     GoogleMap(
         modifier = Modifier.fillMaxSize(),
         cameraPositionState = cameraPositionState
     ) {
         Marker(
-            state = MarkerState(position = singapore),
+            state = MarkerState(position = userLocation),
             title = "Singapore",
             snippet = "Marker in Singapore"
         )
