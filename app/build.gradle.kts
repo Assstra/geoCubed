fd0a8f2479db273fd29986d9add42e9baf35d2c6
Adrien Raimbault <adrien.raimbault@etu.umontpellier.fr>
2024-06-16 18:41:21 +0200

    misc: add retrofit2 for API calls

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,38 +47,40 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
-    implementation("androidx.compose.foundation:foundation:1.6.4")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.1")
+    implementation("androidx.compose.foundation:foundation:1.6.7")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
 
     implementation("androidx.compose.material3:material3:1.2.1")
 
-    implementation("androidx.activity:activity-compose:1.8.2")
-    implementation("androidx.compose.ui:ui:1.6.4")
+    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation("androidx.compose.ui:ui:1.6.7")
     implementation("androidx.navigation:navigation-compose:2.7.7")
 
+    // Maps & Location
     implementation ("com.google.maps.android:maps-compose:4.4.0")
     implementation("com.google.android.gms:play-services-maps:18.2.0")
-
-    // Optionally, you can include the Compose utils library for Clustering,
-    // Street View metadata checks, etc.
-    //implementation ("com.google.maps.android:maps-compose-utils:4.4.1")
-
-    // Optionally, you can include the widgets library for ScaleBar, etc.
-    //implementation ("com.google.maps.android:maps-compose-widgets:4.4.1")
-
-    implementation("com.google.android.gms:play-services-location:21.2.0")
+    implementation("com.google.android.gms:play-services-location:21.3.0")
+    implementation ("com.google.maps.android:maps-utils-ktx:1.7.0")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-
     implementation ("androidx.activity:activity-ktx:1.9.0")
-    implementation ("androidx.fragment:fragment-ktx:1.6.2")
     implementation("androidx.compose.ui:ui-tooling-preview-android:1.6.7")
+
+    // ARCore
+    // implementation ("com.google.ar:core:1.43.0")
+    implementation ("io.github.sceneview:arsceneview:2.2.0")
+
+    // Retrofit
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.7.1")
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.11.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
