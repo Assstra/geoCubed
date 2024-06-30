b8231d7e7fb7fd7f60ca409d359a93dfddde5864
Adrien Raimbault <adrien.raimbault@etu.umontpellier.fr>
2024-06-30 22:18:23 +0200

    misc: stuff

--- app/src/main/java/com/araimbault/geocubed/MainActivity.kt
+++ app/src/main/java/com/araimbault/geocubed/MainActivity.kt
@@ -7,13 +7,18 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatDelegate.setApplicationLocales
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.core.content.ContextCompat
 import androidx.navigation.compose.rememberNavController
+import com.araimbault.geocubed.data.SearchLocationViewModel
+import com.araimbault.geocubed.data.UserLanguageViewModel
+import com.araimbault.geocubed.data.UserLocationViewModel
 import com.araimbault.geocubed.ui.components.navigation.NavController
 import com.araimbault.geocubed.ui.theme.GeoCubedTheme
+//import com.araimbault.geocubed.utils.ARCoreSessionLifecycleHelper
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 
@@ -26,41 +31,48 @@ class MainActivity : ComponentActivity() {
             permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
                 // Precise location access granted.
                 Toast.makeText(this, "Precise location access granted.", Toast.LENGTH_SHORT).show()
-                // TODO: Add logic to handle precise location access
             }
 
             permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
                 // Only approximate location access granted.
                 Toast.makeText(this, "Approximate location access granted.", Toast.LENGTH_SHORT)
                     .show()
-                // TODO: Add logic to handle approximate location access
+            }
+
+            permissions.getOrDefault(Manifest.permission.CAMERA, false) -> {
+                // Camera usage granted.
+                Toast.makeText(this, "Camera usage granted.", Toast.LENGTH_SHORT)
+                    .show()
             }
 
             else -> {
                 // No location access granted.
                 Toast.makeText(this, "No location access granted.", Toast.LENGTH_SHORT).show()
-                // TODO: Add logic to handle no location access
             }
         }
     }
 
-    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var fusedLocationClient: FusedLocationProviderClient;
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         checkLocationPermissions()
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
+        val userLocationViewModel = UserLocationViewModel(fusedLocationClient)
+        val searchLocationViewModel = SearchLocationViewModel()
+        val userLanguageViewModel = UserLanguageViewModel()
+
         super.onCreate(savedInstanceState)
         setContent {
             val navController = rememberNavController()
-
             GeoCubedTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
                     NavController(
                         navController = navController,
-
-                        fusedLocationClient,
+                        userLocationViewModel,
+                        searchLocationViewModel,
+                        userLanguageViewModel,
                     )
                 }
             }
@@ -73,7 +85,6 @@ class MainActivity : ComponentActivity() {
                 // Precise location access granted.
                 Toast.makeText(this, "Precise location access already granted.", Toast.LENGTH_SHORT)
                     .show()
-                // TODO: Add logic to handle already granted precise location access
             }
 
             ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) -> {
@@ -83,14 +94,23 @@ class MainActivity : ComponentActivity() {
                     "Approximate location access already granted.",
                     Toast.LENGTH_SHORT
                 ).show()
-                // TODO: Add logic to handle already granted approximate location access
+            }
+
+            ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) -> {
+                // Camera permission denied.
+                Toast.makeText(
+                    this,
+                    "Camera permission already granted",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
 
             else -> {
                 locationPermissionRequest.launch(
                     arrayOf(
                         Manifest.permission.ACCESS_FINE_LOCATION,
-                        Manifest.permission.ACCESS_COARSE_LOCATION
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.CAMERA
                     )
                 )
             }
